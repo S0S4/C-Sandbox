3b1e7a9464d85ab034198acce21174a8f457e550
S0S4 <hidden>
2023-05-15 11:24:41 -0400

    Hackerrank C Challenges

--- HackerRank-Challenges/Max.c
+++ HackerRank-Challenges/Max.c
@@ -6,11 +6,16 @@ int max_of_four(int a, int b, int c, int d){
     int numbers[4] = {a, b, c, d};
     int bigNumber = 0;
 
-    for(int i = 0; i < 3; i++){
+    for(int i =0; i < 4; i++){
 
-        bigNumber = numbers[i] > numbers[i+1] ? numbers[i] : numbers[i+1];
+       if(numbers[i] > numbers[i+1]){
+
+           numbers[i] = bigNumber;
+       } else{
+           numbers[i+1] = bigNumber;
+       }
+
     }
-
     return bigNumber;
 }
 int main() {
